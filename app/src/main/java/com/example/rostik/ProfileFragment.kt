df73a12f4c7418efa236a711b3b6a32caf50008b
Dmitrii Glushkov <glushkov.dimaz2000@yandex.ru>
2021-06-02 22:40:08 +0700

    Made profile fragment

--- app/src/main/java/com/example/rostik/ProfileFragment.kt
+++ app/src/main/java/com/example/rostik/ProfileFragment.kt
@@ -6,26 +6,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.rostik.databinding.FragmentProfileBinding
+import com.example.rostik.domain.account.AccountEntity
+import com.example.rostik.domain.type.None
+import com.example.rostik.presentation.viewmodel.AccountViewModel
+import com.example.rostik.ui.core.BaseFragment
+import com.example.rostik.ui.core.ext.onFailure
+import com.example.rostik.ui.core.ext.onSuccess
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : BaseFragment() {
 
     companion object {
         fun newInstance() = ProfileFragment()
     }
 
-    private lateinit var viewModel: ProfileViewModel
+    private lateinit var accountViewModel: AccountViewModel
+    override lateinit var layout: View
+    private lateinit var binding:FragmentProfileBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        layout = binding.root
+
+        accountViewModel = viewModel {
+            onSuccess(accountData, ::handleAccount)
+            onSuccess(logoutData, ::handleLogout)
+            onFailure(failureData, ::handleFailure) }
+
+        accountViewModel.getAccount()
+
+        binding.buttonLogout.setOnClickListener {
+            accountViewModel.logout()
+        }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun handleAccount(accountEntity: AccountEntity?) {
+        accountEntity?.let {
+            binding.tvProfile1.text = "${it.F}\n${it.I}\n" +
+                    "${it.O}"
+            binding.tvProfile2.text = it.login
+            binding.tvProfile3.text = it.car
+            binding.tvProfile4.text = it.phone
+        }
     }
 
+    private fun handleLogout(none: None?) {
+        navigator.showLogin(requireContext())
+        activity?.finish()
+    }
 }
